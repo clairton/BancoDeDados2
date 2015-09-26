6e9197325978c014cbdfd6f0ac2177d290ed93c9
Rodrigo <coloradonanet@hotmai.com>
2015-09-26 09:41:09 +0800

    Questao 2

--- Recuperaco.sql
+++ Recuperaco.sql
@@ -1,4 +1,9 @@
 1 - SELECT SUM (invoices.total) as total from invoices
 
+2 - SELECT items.quantity, customers.street from items
+inner join invoices on invoices.id = items.invoice_id
+inner join customers on customers.id = invoices.customer_id
+order by items.quantity asc
+
 
 
