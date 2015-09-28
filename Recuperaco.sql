0f8f1c0da91ac62209b3c179545c15c0ab9003be
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2015-09-28 12:32:50 -0300

    Inicio correção

--- Recuperaco.sql
+++ Recuperaco.sql
@@ -3,16 +3,29 @@
 2 - SELECT items.quantity, customers.street from items
 inner join invoices on invoices.id = items.invoice_id
 inner join customers on customers.id = invoices.customer_id
+group by customers.street,items.quantity
 order by items.quantity asc
 
 
-3 - SELECT (products.name), customers.city, max (invoices.total) from products
+3 - SELECT products.name, customers.city, max (items.cost * items.quantity) from products
 left join items on items.product_id = items.id
 left join invoices on invoices.id = items.invoice_id
 left join customers on customers.id = invoices.customer_id
 group by
 products.name,
 invoices.total,
+customers.city
+order by customers.city asc
+
+
+SELECT products.id, products.name, customers.city, sum(items.cost * items.quantity) as total
+from products
+left join items on items.product_id = items.id
+left join invoices on invoices.id = items.invoice_id
+left join customers on customers.id = invoices.customer_id
+group by
+products.id,
+products.name,
 customers.city
 order by customers.city asc
 
